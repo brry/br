dc992413360b8a2e6bae3397b95a69c447628145
brry <berryboessenkool@hotmail.com>
2015-06-24 08:18:17 +0200

    installB: slightly updated

--- R/installB.R
+++ R/installB.R
@@ -1,5 +1,5 @@
-# shortcut for developing my packgage
-# Berry Boessenkool, Dec 2014
+# shortcut for developing my package
+# Berry Boessenkool, berry-b@gmx.de, Dec 2014
 
 installB <- function(
 package="berryFunctions", # package name
@@ -12,13 +12,14 @@ if(!file.exists(path)) { substr(path, 1,1) <- "~" ; substr(path, 2,2) <- "" }
 #
 package <- deparse(substitute(package))
 package <- gsub("\"", "", package, fixed=TRUE)
-if(package=="2") package <- "extremeStat"
 # remove function objects from workspace
 d <- dir(paste0(path, "/", package, "/R"))
 d <- gsub(".r", "", d, fixed=TRUE)
 d <- gsub(".R", "", d, fixed=TRUE)
 l <- ls(globalenv())
 rm(list=l[l %in% d], envir=globalenv())
+# unload package and dependencies to avoid "unloading not successfull" messages
+#.....
 # install
 devtools::install(paste0(path, "/", package))
 library(package, character.only=TRUE)
