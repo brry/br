f4333d8f7f19e4da3ab56e8f60ccbd11c4799bb5
brry <berryboessenkool@hotmail.com>
2015-06-24 08:18:39 +0200

    installE and updataPackage added

---
+++ R/installE.R
@@ -0,0 +1,11 @@
+# shortcut for developing my package
+# Berry Boessenkool, berry-b@gmx.de, June 2015
+
+# \code{\link{installB}} with default package extremeStat
+
+installE <- function(
+package="extremeStat", # package name
+path="S:/Dropbox/Public") # path containing package folder
+{
+installB(package, path)
+}
