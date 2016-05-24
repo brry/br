b297cd81a9ed2e509cc32624b4f5c8210d9b4bd6
brry <berryboessenkool@hotmail.com>
2016-05-24 17:42:10 +0200

    updatePackage: path controlled, loadAndMessage used

--- R/updatePackage.R
+++ R/updatePackage.R
@@ -1,42 +1,44 @@
-# install current development version of package from folder
+#' install package from local drive
+#'
+#' install current development version of package from folder
+#'
+#' @details Calls \code{\link{installB}} if the package date or version in the source code has been changed.
+#'
+#' @return Nothing
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, 2014
+#' @seealso \code{\link{installB}}, \code{\link{packageDescription}}, \code{\link{read.dcf}}
+#' @keywords package
+#' @export
+#' @examples
+#' # See ?loadPackages
+#'
+#' @param package Package name. DEFAULT: "berryFunctions"
+#' @param path Path containing package folder. DEFAULT: "S:/Dropbox/Public"
+#'
 updatePackage <- function(
-package="berryFunctions", # package name
-path="S:/Dropbox/Public" # path containing package folder
+package="berryFunctions",
+path="S:/Dropbox/Public"
 )
-{
 if(interactive())
 {
 if(!requireNamespace(package, quietly=TRUE))
   stop("package ", package, " must be installed before updatePackage can work.")
 # remove end slash
-if(substring(path, nchar(path))=="/")
+while(substring(path, nchar(path))=="/")
    path <- substring(path, 1, nchar(path)-1)
+# laptop linux path change:
+if(!file.exists(path)) path <- gsub("S:", "~", path)
+# work PC path change:
+if(!file.exists(path)) path <- gsub("~", "C:/Users/boessenkool", path)
+# path control
+if(!file.exists(path)) stop("path does not exist. ", path)
 # installed date/version:
 Vinst <- utils::packageDescription(package)[c("Date","Version")]
 # date in source code
 Vsrc <- read.dcf(file=paste0(path,"/",package, "/DESCRIPTION"), fields=c("Date","Version"))
 # install if outdated:
 if( as.Date(Vsrc[,"Date"]) > as.Date(Vinst$Date) | Vsrc[,"Version"] > Vinst$Version)
-  {
-  require("devtools", quietly=TRUE) # now updatePackage can be in the Rprofile.site startup routine
-  require("utils", quietly=TRUE) # to avoid warning: could not find function "available.packages"
-  require("stats", quietly=TRUE) # the same for update
   installB(package, path)
-  }
 # load:
-require(package, character.only=TRUE, quietly=TRUE)
-# prepare message
-version <- utils::packageDescription(package)$Version
-date <- utils::packageDescription(package)$Date
-message("Loaded package ", package, ", Version ", version, " from ", date ,".")
+loadAndMessage(package)
 }
-}
-
-# Example Rprofile.site
-#if(interactive())
-#{
-#library(installB)
-#cat("-----------------------------------------------\n")
-#updatePackage("berryFunctions", path="D:/Dropbox/Public")
-#updatePackage(   "extremeStat", path="D:/Dropbox/Public")
-#}
