b880f8a422ef79249f3272f30d94162254f92824
brry <berryboessenkool@hotmail.com>
2017-10-22 23:41:45 +0200

    packsNewR: dependencies installed as such to avoid duplicate installations

--- R/installB.R
+++ R/installB.R
@@ -331,36 +331,38 @@ isInstalled <- function(package)
 #' @param \dots Arguments passed to \code{\link{install.packages}}
 packsNewR <- function(...)
 {
+if(!isInstalled("pbapply")) install.packages("pbapply")
+
 packs <- c("RColorBrewer", "berryFunctions", "rdwd", "foreign", "RCurl",
 "zoo", "TeachingDemos", "ade4", "data.table", "microbenchmark", "nortest", "plotrix",
 "rgl", "xts", "zoom", "zyp", "gstat", "numbers", "readxl", "lattice",
 "gtools", "ncdf4", "pbapply",
 "knitr", "devtools","rmarkdown", "roxygen2", "testthat", "extremeStat",
-
 "rgdal", "rJava", "rgeos", "spatstat", "OSMscale", "geoR", "mapdata", "maps",
 "raster", "RandomFields",
 "maptools", "leaflet", "mapview", "sf", "dygraphs", "sp", "animation", "ggplot2",
 "hexbin", "jpeg", "png", "rstudioapi"
 )
-
-message("Getting package dependencies ...")
-basepacks <- c("base", "compiler", "datasets", "grDevices",
-        "graphics", "grid", "methods", "parallel", "profile", "splines",
-        "stats", "stats4", "tcltk", "tools", "translations", "utils")
-deps <- tools::package_dependencies(packs, recursive=TRUE)
-deps <- unique(unlist(deps))
-deps <- deps[!deps %in% basepacks]
-packs <- unique(c(deps,packs))
-if(!isInstalled("pbapply")) install.packages("pbapply")
-
-message("Checking ",length(packs)," packages for installation ...")
-inst <- pbapply::pbsapply(packs, isInstalled) # [1:which(packs=="OpenStreetMap")]
+message("Checking ",length(packs)," packages if they are installed ...")
+inst <- pbapply::pbsapply(packs, isInstalled) 
 
 if(any(!inst))
   {
-  message("installB::packsNewR will install ",sum(!inst)," packages ...")
+  message("Getting package dependencies ...")
+  deps <- tools::package_dependencies(packs[!inst], recursive=TRUE)
+  deps <- unique(unlist(deps))
+  basepacks <- c("base", "compiler", "datasets", "grDevices",
+          "graphics", "grid", "methods", "parallel", "profile", "splines",
+          "stats", "stats4", "tcltk", "tools", "translations", "utils")
+  deps <- deps[!deps %in% c(basepacks,packs)]
+  message("Checking ",length(packs)," dependencies if they are installed ...")
+  deps <- pbapply::pbsapply(deps, isInstalled) 
+  
+  install_if_needed <- function(p) if(!isInstalled(p)) install.packages(p, ...)
+  message("installB::packsNewR will install ",sum(!inst)," packages with ",
+          sum(!deps), " dependencies ...")
   Sys.sleep(1) # show message longer
-  pbapply::pbsapply(packs[!inst], install.packages, ...)
+  pbapply::pbsapply(packs[!inst], install_if_needed, ...)
   } else
   message("All packages listed in installB::packsNewR are installed.")
 
