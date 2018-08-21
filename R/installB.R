f73792d3e37c795724574af7ede7771416480aa6
brry <berryboessenkool@hotmail.com>
2018-08-21 08:00:52 +0200

    installA: can now handle non-git repositories

--- R/installB.R
+++ R/installB.R
@@ -142,12 +142,21 @@ packs <- packs[packs!="shapeInteractive"]
 for(p in packs) installB(package=p, path=path, quiet=quiet, load=FALSE, ...)
 for(p in packs) installB(package=p, path=path, quiet=quiet, unloadrevdep=FALSE, ...)
 # check for unstaged git changes:
+message("-----")
+
 for(p in packs)
 {
-r <- git2r::repository(file.path(path, p), discover = TRUE)
-st <- unlist(git2r::status(r))
-if(!is.null(st)) message(length(st), " unstaged changes in ",format(p,width=15),
+r <- try(git2r::repository(file.path(path, p), discover = TRUE), silent=TRUE)
+if(inherits(r, "try-error")) 
+ {
+ if(p!="check") message("not a git repository: ", p) 
+ }
+else
+ {
+ st <- unlist(git2r::status(r))
+ if(!is.null(st)) message(length(st), " unstaged changes in ",format(p,width=15),
                          ": ", toString(st))
+ }
 }
 # getting uncommited changes via devtoools:::git_sync_status -> git2r::fetch
 # fails due to failing ssh authentification
@@ -164,6 +173,7 @@ st <- unlist(git2r::status(r))
 if(!is.null(st)) message(length(st), " unstaged changes in ",format(p,width=15),
                          ": ", toString(st))
 }
+message("-----")
 }
 
 
