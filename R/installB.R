b469c3add758485041e3e983af386c3a301c0fc5
brry <berryboessenkool@hotmail.com>
2019-01-11 03:00:39 +0100

    course function added

--- R/installB.R
+++ R/installB.R
@@ -195,6 +195,24 @@ pathFinder <- function(path="S:/Dropbox/Rpack") # adjust path based on computer 
   path
 }
 
+
+
+# course -------------------------------------------------------------------
+
+#' @title open R course PDF
+#' @description open R course PDF in default viewer
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jan 2019
+#' @export
+#' @param pres Logical: Open presentation file with LaTeX onslide et al? DEFAULT: FALSE
+
+course <- function(pres=FALSE) 
+{
+ path <- sub("/Rpack", "/R/course/RcourseBerry.pdf", pathFinder()  )
+ if(pres) path <- sub(".pdf", " pres.pdf", path)
+ berryFunctions::openFile(path)
+}
+
+
 # loadAndMessage ---------------------------------------------------------------
 
 #' @export
