17c8f034e7946f677523d5676a32a6785aefc789
brry <berryboessenkool@hotmail.com>
2019-06-07 12:18:07 +0200

    added function profilesite

--- R/installB.R
+++ R/installB.R
@@ -216,6 +216,20 @@ course <- function(pres=FALSE)
 }
 
 
+# profilesite -------------------------------------------------------------------
+
+#' @title open Rprofile.site
+#' @description open Rprofile.site in default viewer
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jun 2019
+#' @export
+profilesite <- function() 
+{
+ path <- paste0( R.home("etc"), "/Rprofile.site")
+ berryFunctions::openFile(path)
+}
+
+
+
 # loadAndMessage ---------------------------------------------------------------
 
 #' @export
