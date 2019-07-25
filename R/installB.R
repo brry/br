b570f80c38a984dc74ebc5248e5c66af7d9c050e
brry <berryboessenkool@hotmail.com>
2019-07-25 08:51:30 +0200

    newsdesc: taken from newsfile to now open both description and news file

--- R/installB.R
+++ R/installB.R
@@ -230,17 +230,19 @@ profilesite <- function()
 
 
 
-# newsfile -------------------------------------------------------------------
+# newsdesc -------------------------------------------------------------------
 
-#' @title open NEWS file
-#' @description open NEWS file in default viewer
+#' @title open NEWS and DESCRIPTION files at package root
+#' @description open NEWS and DESCRIPTION files at package root in default viewer
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jul 2019
 #' @export
-#' @param path Filename at pacakge root directory to be opened
-newsfile <- function(path="NEWS") 
+#' @param path Package dir. DEFAULT: "."
+newsdesc <- function(path=".") 
 {
-path <- berryFunctions::packagePath(file=path)
-file.edit(path) # for the Rstudio version, don't call utils::file.edit explicitely
+path <- berryFunctions::packagePath(path)
+# for the Rstudio version, don't call utils::file.edit explicitely
+file.edit(paste0(path,"/DESCRIPTION")) 
+file.edit(paste0(path,"/NEWS")) 
 return(invisible(path))
 }
 
