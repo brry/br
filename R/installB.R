89a5036273f0ee111f05dbce9a641751d474797f
brry <berryboessenkool@hotmail.com>
2019-10-30 01:24:08 +0100

    newsdesc: in rdwd, also opens the vign source code

--- R/installB.R
+++ R/installB.R
@@ -243,6 +243,7 @@ path <- berryFunctions::packagePath(path)
 # for the Rstudio version, don't call utils::file.edit explicitely
 file.edit(paste0(path,"/DESCRIPTION")) 
 file.edit(paste0(path,"/NEWS")) 
+if(grepl("rdwd", path)) file.edit(paste0(path, "/misc/vign/index.Rmd"))
 return(invisible(path))
 }
 
