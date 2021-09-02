976a4dbeb2b3c2291f71c0a4ce2f6f0230b38a6e
brry <berryboessenkool@hotmail.com>
2021-09-02 15:45:31 -0400

    del_ds: initial release

--- R/installB.R
+++ R/installB.R
@@ -653,3 +653,18 @@ convert_crlf_for_git_on_mac <- function(folder=".", infile=NULL, ...)
 {
 stop("Just set:   git config --global core.autocrlf input")
 }
+
+
+# del_ds --------------------------------------------------
+
+#' @title delete .DS_Store file on Mac OS
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Sept 2021
+#' @export
+#' @param folder  Folder in which all (incl recursive) .DS_Store files shall be deleted 
+del_ds <- function(folder=".")
+{
+f <- dir(folder, pattern="^\\.DS_Store$", recursive=TRUE, full.names=TRUE, all.files=TRUE)
+file.remove(f)
+f
+}
+
